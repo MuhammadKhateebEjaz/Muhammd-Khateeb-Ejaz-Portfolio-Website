a185203e4a3e16f5d7d954e12ed0f09d3db4d28c
Muhammad Khateeb Ejaz <160773008+MuhammadKhateebEjaz@users.noreply.github.com>
2025-08-16 13:29:53 -0700

    Update and rename script.js to app.js

---
+++ portfolio/app.js
@@ -0,0 +1,20 @@
+
+//AOS
+
+AOS.init({duration:1000, once:true});
+
+//menu toggle
+
+function toggleMenu(){
+    document.getElementById("navLinks").classList.toggle("active");
+}
+
+
+//typing effect
+
+new Typed("#typing", {
+    strings: ['Web Developer.' ,  'Web Designer.' , 'UI/UX Designer.', 'Crative Thinker.'],
+    typeSpeed:60, backSpeed: 30, backDelay:1000, loop:true
+});
+
+ 
